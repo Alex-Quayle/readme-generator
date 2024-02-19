6fa07909051645ca8fc7e9dd0ecf05c175e6feeb
Alex-Quayle <151681817+Alex-Quayle@users.noreply.github.com>
2024-02-19 13:02:16 +0000

    Added questions.

--- index.js
+++ index.js
@@ -5,7 +5,57 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your README',
+      },
+      {
+        type: 'input',
+        message: 'Please enter a description of your project',
+        name: 'desc',
+      },
+      {
+        type: 'input',
+        message: 'Please provide installation instructions for the project',
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: 'Please provide usage instructions for the project',
+        name: 'usage',
+      },
+      {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'contact',
+        choices: ['email', 'phone', 'telekinesis'],
+      },
+      {
+        type: 'input',
+        message: 'Please provide a name of a contributor',
+        name: 'contributor',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter testing information',
+      },
+      {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Please enter your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+      },
+      {
+        type: 'input',
+        name: 'comms',
+        message: 'Please enter communication details',
+      }
 ];
 
 // function to write README file
@@ -14,8 +64,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
 }
 
 // function call to initialize program
 init();
+
+// ADD MARKDOWN
